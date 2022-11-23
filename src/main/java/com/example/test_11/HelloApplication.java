17ba8bf98c54f0230ae43823e7eb33d65c9484eb
songyue <374037464@qq.com>
2022-11-23 22:36:34 +0800

    111

---
+++ src/main/java/com/example/test_11/HelloApplication.java
@@ -0,0 +1,9 @@
+package com.example.test_11;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("/api")
+public class HelloApplication extends Application {
+
+}
